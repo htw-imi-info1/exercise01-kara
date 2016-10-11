bc8daaf451e17e1b98343ce5c27c586b1369ec97
B Kleinen <kleinen@infrastructure.de>
2016-10-11 16:03:09 +0200

    binary completed

--- lab01_kara07/Kara06.java
+++ lab01_kara07/Kara06.java
@@ -7,46 +7,51 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Kara06 extends Kara{
-    
+
     int expectedResult = 0;
     int karasResult = 0;
-    
+
     public Kara06(int expectedResult){
         super();
         this.expectedResult = expectedResult;
     }
-    
-        /**
-         * Act - do whatever the Kara06 wants to do. This method is called whenever
-         * the 'Act' or 'Run' button gets pressed in the environment.
-         */
 
-        public void act() 
-        {
-            if (mushroomFront()) {
-                checkResult();
-                Greenfoot.stop();
-            }
-            
+    /**
+     * Act - do whatever the Kara06 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+
+    public void act() 
+    {
+        if (mushroomFront()) {
+            checkResult();
+            Greenfoot.stop();
+        } else {
             // Add your action code here.
-        }  
-        public void checkResult(){
-            turnRight();
-            turnRight();
-            int karasResult = 0;
-            for (int stelle = 7;stelle>= 0;stelle--){
-                move();
-                if (onLeaf()){
-                    karasResult += Math.pow(2,stelle);
-                }
-            }
             move();
-            if (expectedResult != karasResult){
-                System.out.println("Kara hat nicht richtig gerechnet!");
-                System.out.println("Karas Ergebnis: "+karasResult+", es hätte sein sollen: "+expectedResult);
-                System.out.println("Kara: "+Integer.toBinaryString(karasResult)+ " expected: "+Integer.toBinaryString(expectedResult));
-            }
-        
+        }
+    }  
+
+    public void checkResult(){
+        turnRight();
+        turnRight();
+        int karasResult = 0;
+        for (int stelle = 8;stelle>= 0;stelle--){
+            move();
+            if (onLeaf()){
+                karasResult += Math.pow(2,stelle);
             }
         }
-    
+        move();
+        if (expectedResult != karasResult){
+            System.out.println("Kara hat nicht richtig gerechnet!");
+            System.out.println("Karas Ergebnis: "+karasResult+", es hätte sein sollen: "+expectedResult);
+            System.out.println("Kara: "+Integer.toBinaryString(karasResult)+ " expected: "+Integer.toBinaryString(expectedResult));
+        } else {
+            System.out.println("Kara hat richtig gerechnet! "+ Integer.toBinaryString(karasResult));
+        }
+        
+
+    }
+}
+
